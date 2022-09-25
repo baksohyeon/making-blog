707258f1004a1ee68e53ca67841e4dcbd2967a70
baksohyeon <bori12370@gmail.com>
2022-09-25 18:00:35 +0900

    feat: add validation about checking empty, string

--- src/blog/dto/create-board.dto.ts
+++ src/blog/dto/create-board.dto.ts
@@ -1,10 +1,20 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateBoardDto {
+  @IsNotEmpty()
+  @IsString()
   readonly title: string;
 
+  @IsNotEmpty()
+  @IsString()
   readonly description: string;
 
+  @IsNotEmpty()
+  @IsString()
   readonly body: string;
 
+  @IsNotEmpty()
+  @IsString()
   readonly author: string;
 }
 

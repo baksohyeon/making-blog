53fd2c69aa853f11b31ce4faef1f8a38a4054003
baksohyeon <bori12370@gmail.com>
2022-09-30 18:28:28 +0900

    feat: making validate-board middleware structor

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,12 +1,24 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './entity/board.entity';
+import { ValidateBoardMiddleware } from './middlewares/validate-board.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Board])],
   providers: [BlogService],
   controllers: [BlogController],
 })
-export class BlogModule {}
+export class BlogModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateBoardMiddleware)
+      .forRoutes({ path: 'blog', method: RequestMethod.GET });
+  }
+}

af44d8b47d55c8136b66b49987dc2c0fb683274a
baksohyeon <bori12370@gmail.com>
2022-10-12 15:59:25 +0900

    fix: 체리픽하면서 실수로 딸려온 파일 제거

--- src/blog/pipes/validation.pipe.ts
+++
@@ -1,5 +0,0 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
-
-export class ApiTokenCheckMiddleware implements PipeTransform {
-  async transform(value: any, metadata: ArgumentMetadata) {}
-}

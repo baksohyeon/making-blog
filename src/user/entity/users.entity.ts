4321d61c35cab2d0782eea003cdbc88490f465ab
baksohyeon <bori12370@gmail.com>
2022-10-14 14:54:24 +0900

    feature: add hashedPassword  feature and amend bio, image Column

--- src/user/entity/users.entity.ts
+++ src/user/entity/users.entity.ts
@@ -1,5 +1,12 @@
 import { IsEmail, isEmail, IsNotEmpty, Length } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Entity()
 export class Users {
@@ -21,10 +28,20 @@ export class Users {
   @IsNotEmpty({ message: 'The password is required' })
   password: string;
 
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+
   @IsEmail({ unique: true })
   @Column()
   email: string;
 
-  @Column()
+  @Column({ default: '' })
   bio?: string;
+
+  @Column({ default: '' })
+  image: string;
 }

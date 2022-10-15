5779004ec22cb46cc3be691ac3c7be5ee1d0c805
baksohyeon <bori12370@gmail.com>
2022-10-15 13:19:31 +0900

    feat: amend board entity to fit project  requiring api

--- src/board/entity/board.entity.ts
+++ src/board/entity/board.entity.ts
@@ -6,35 +6,39 @@ import {
   Generated,
   PrimaryColumn,
   PrimaryGeneratedColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity()
 export class Board {
-  // @PrimaryColumn({ type: 'binary', length: 16 })
-  // id!: Buffer;
   @PrimaryColumn({ type: 'binary' })
   @Generated('uuid')
   id: string;
 
   @Column()
+  slug: string;
+
+  @Column()
   title: string;
 
-  @Column()
+  @Column({ default: '' })
   description: string;
 
-  @Column()
+  @Column({ default: '' })
   body: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 255 })
   username: string;
 
-  // @CreateDateColumn({ type: 'timestamp' })
-  // date_posted: Date;
+  @CreateDateColumn()
+  createdAt: Date;
 
-  @Column({
-    name: 'dated_at',
-    type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  datedAt?: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column({ type: 'simple-array' })
+  tagList: string[];
+
+  @Column({ default: 0 })
+  favoritesCount: number;
 }

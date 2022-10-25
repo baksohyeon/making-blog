233b4ff42d79b0de55ce85633d42920467276530
baksohyeon <bori12370@gmail.com>
2022-10-25 15:42:24 +0900

    fix: change jwt secert key using env file

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.APP_SECRET,
+      secretOrKey: process.env.APP_SECERT,
     });
   }
 
